898a66f1186cbae8bff40655e949817255640f3d
Luis Pineda <luisen.p@gmail.com>
2014-10-20 12:33:47 -0400

    Added some utility methods for generating combinations

--- include/util/general.h
+++ include/util/general.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <unistd.h>
 #include <chrono>
+#include <vector>
 #include <thread>
 
 #define dprint1(x) std::cerr << x << std::endl
@@ -13,9 +14,67 @@
 #define dprint3(x,y,z) std::cerr << x << " "  << y << " " << z << std::endl
 #define dprint4(x,y,w,z) std::cerr << x << " "  << y << " " << w << " " << z << std::endl
 
+/**
+ * Sleeps the current thread for the given number of milliseconds. This is just a wrapper for
+ * function std::this_thread::sleep_for(std::chrono milliseconds).
+ */
 inline void dsleep(int miliseconds) {
     std::this_thread::sleep_for(std::chrono::milliseconds(miliseconds));
 }
+
+/**
+ * Computes the next combination (no repetitions) of numbers from 0 to (n-1) chosen k at a time.
+ * The method returns true unless no more combinations are possible.
+ *
+ * This method modifies the given vector in place.
+ *
+ * For instance, calling nextComb(<0,2>, 3, 2) stores the vector <1,0> and returns true,
+ * while calling nextComb(<1,2>, 3, 2) returns false.
+ */
+inline bool nextComb(std::vector<int>& comb, int n, int k)
+{
+    int i = k - 1;
+    while (i >= 0) {
+        if (comb[i] == n - k + i)
+            i--;
+        else
+            break;
+    }
+    if (i == -1)
+        return false;
+    comb[i]++;
+    for (int j = i + 1; j < k; j++)
+        comb[j] = comb[j - 1] + 1;
+    return true;
+}
+
+
+/**
+ * Computes the next combination (with repetitions) of n elements with k options for each.
+ * The method returns true unless no more combinations are possible.
+ *
+ * This method modifies the given vector in place.
+ *
+ * For instance, calling nextComb(<1,0>, 2) stores the vector <1,1> and returns true,
+ * while calling nextComb(<1,1>, 2) returns false.
+ */
+inline bool nextCombRep(std::vector<int>& comb, int k)
+{
+    int j = comb.size() - 1;
+    while (j >= 0) {
+        comb[j]++;
+        if (comb[j] == k) {
+            comb[j] = 0;
+            j--;
+        } else {
+            break;
+        }
+    }
+    if (j == -1)
+        return false;
+    return true;
+}
+
 
 
 struct pair_int_equal {
