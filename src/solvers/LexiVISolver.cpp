9e5a94cbd528b058cb09712570af8bde0885ff9f
Luis Pineda <luisen.p@gmail.com>
2015-01-19 21:39:52 -0500

    more debug code cleaning up

--- src/solvers/LexiVISolver.cpp
+++ src/solvers/LexiVISolver.cpp
@@ -25,23 +25,8 @@ mlcore::Action* LexiVISolver::solve(mlcore::State* s0)
     for (int i = 0; i < maxIter_; i++) {
         double maxResidual = 0.0;
         for (mlcore::State* s : problem_->states()) {
-
-
-//                mllexi::LexiRacetrackState* rts = (mllexi::LexiRacetrackState*) s;
-//                if ((rts->x() == 31 && rts->y() == 29 && rts->vx() == 0 && rts->vy() == 0)
-//                   || (rts->x() == 29 && rts->y() == 31 && rts->vx() == 3 && rts->vy() == -1))
-//                    mdplib_debug = true;
-//                else
-//                    mdplib_debug = false;
-//
-//            dprint2(" ***** STATE ********** ", s);
-
             double residual = lexiBellmanUpdate((mllexi::LexiProblem *) problem_,
                                                  (mllexi::LexiState *) s);
-
-//            dprint3(rts->lexiCost()[0]," ", rts->lexiCost()[1]);
-//            dprint1(residual);
-
             if (residual > maxResidual)
                 maxResidual = residual;
         }
