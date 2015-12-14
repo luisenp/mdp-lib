ba3834443a6ff868964bd8636ce670b15c6a56e6
Luis Pineda <luisen.p@gmail.com>
2015-12-14 13:12:45 -0500

    added a script to run the metareasoning simulator experiments

--- scripts/runmeta.sh
+++ scripts/runmeta.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=../data/gws/map4.gw --meta=assume1 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
-# done
-# 
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=../data/gws/map4.gw --meta=assume2 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
-# done
-
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=../data/gws/map4.gw --meta=assume1 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i --all_actions
-# done
+for i in `seq 1 20`;
+do
+  ../simulmeta --grid=../data/gws/map5.gw --meta=assume1 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
+done
 
 for i in `seq 1 20`;
 do
-  ../simulmeta --grid=../data/gws/map4.gw --meta=assume2 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i --all_actions
+  ../simulmeta --grid=../data/gws/map5.gw --meta=assume2 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
 done
 
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=../data/gws/map4.gw --meta=nometa --nsims=10000 --v=0 --steps_action=$i --steps_nop=1
-# done
+for i in `seq 1 20`;
+do
+  ../simulmeta --grid=../data/gws/map5.gw --meta=assume1 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i --all_actions
+done
+
+for i in `seq 1 20`;
+do
+  ../simulmeta --grid=../data/gws/map5.gw --meta=assume2 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i --all_actions
+done
+
+for i in `seq 1 20`;
+do
+  ../simulmeta --grid=../data/gws/map5.gw --meta=nometa --nsims=10000 --v=0 --steps_action=$i --steps_nop=1
+done
+
+for i in `seq 1 20`;
+do
+  ../simulmeta --grid=../data/gws/map5.gw --meta=change --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
+done
