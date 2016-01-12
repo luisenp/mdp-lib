f10f214eb00691e4457af9d304b8f0314103c2d5
Luis Pineda <luisen.p@gmail.com>
2016-01-12 15:40:50 -0500

    minor changes

--- scripts/runmeta.sh
+++ scripts/runmeta.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-GRID=../data/gws/map3.gw
+GRID=../data/gws/map6.gw
+NSIMS=100000
+TMAX=10
 
-# for i in `seq 1 20`;
+# # for i in `seq 1 10`;
+# # do
+# #   ../simulmeta --grid=$GRID --meta=assume1 --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=$i
+# # done
+# 
+# for i in `seq 1 10`;
 # do
-#   ../simulmeta --grid=$GRID --meta=assume1 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
+#   ../simulmeta --grid=$GRID --meta=assume2 --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=$i
+# done
+# for i in `seq 1 10`;
+# do
+#   ../simulmeta --grid=$GRID --meta=change --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=$i
 # done
 # 
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=$GRID --meta=assume2 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
-# done
-# 
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=$GRID --meta=assume1 --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i --all_actions
-# done
-
-for i in `seq 1 20`;
+for i in `seq 1 10`;
 do
-  ../simulmeta --grid=$GRID --meta=nometa --nsims=100000 --v=0 --steps_action=$i --steps_nop=1
+  ../simulmeta --grid=$GRID --meta=optimal --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=$i
 done
 
-# for i in `seq 1 20`;
-# do
-#   ../simulmeta --grid=$GRID --meta=change --nsims=10000 --v=0 --steps_action=$i --steps_nop=$i
-# done
-
-for i in `seq 1 20`;
+for i in `seq 1 10`;
 do
-  ../simulmeta --grid=$GRID --meta=optimal --nsims=100000 --v=0 --steps_action=$i --steps_nop=$i
+  ../simulmeta --grid=$GRID --meta=assume1 --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=$i --all_actions
 done
 
-for i in `seq 1 20`;
+for i in `seq 1 10`;
 do
-  ../simulmeta --grid=$GRID --meta=qvimprov --nsims=100000 --v=0 --steps_action=$i --steps_nop=$i
+  ../simulmeta --grid=$GRID --meta=nometa --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=1
+done
+
+for i in `seq 1 10`;
+do
+  ../simulmeta --grid=$GRID --meta=qvimprov --nsims=$NSIMS --v=0 --steps_action=$i --steps_nop=$i
 done
