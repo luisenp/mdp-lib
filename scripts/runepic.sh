ea85ab4e0a5cdce9f068d7b3ad4fc9bc301b1f16
Luis Pineda <luisen.p@gmail.com>
2016-03-10 22:30:41 -0500

    minor changes to the experiments script

--- scripts/runepic.sh
+++ scripts/runepic.sh
@@ -2,13 +2,13 @@
 
 tracks=(known/barto-big-error known/square-4-error known/square-5-error \
         known/ring-5-error known/ring-6-error)
-nsims=100
+nsims=1
 
 for track in ${tracks[@]}; do
   echo "${track}|lrtdp"
   for n in `seq 1 ${nsims}`; do
     ../testsolver.out --track=../data/tracks/$track.track \
-    --algorithm=lrtdp --v=0 --n=1
+    --algorithm=lrtdp --v=0 --n=100 --trials=10
   done
   for horizon in `seq 0 4`; do
     for expansions in `seq 1 3`; do
@@ -16,7 +16,7 @@ for track in ${tracks[@]}; do
       for n in `seq 1 ${nsims}`; do
         ../testsolver.out --track=../data/tracks/$track.track \
         --algorithm=epic --horizon=$horizon --expansions=$expansions \
-        --v=0 --n=1 --trials=100
+        --v=0 --n=100 --trials=10
       done
     done    
   done
