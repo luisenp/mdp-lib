149044609bdab82f05aafe543132e7bd445a28cb
Luis Pineda <luisen.p@gmail.com>
2016-03-23 11:05:37 -0400

    minor changes

--- scripts/runmlrtdp.sh
+++ scripts/runmlrtdp.sh
@@ -7,7 +7,7 @@ nsims=100
 for track in ${tracks[@]}; do
   echo "${track}|lrtdp"
   ../testsolver.out --track=../data/tracks/$track.track \
-  --algorithm=lrtdp --v=0 --n=1 --no-initial-plan \
+  --algorithm=lrtdp --v=0 --n=1 \
   --heuristic=hmin --hmin-solve-all 
   
   echo "${track}|mlrtdp"
@@ -22,7 +22,7 @@ for track in ${tracks[@]}; do
   --heuristic=hmin --hmin-solve-all 
       
   horizon=1
-  for i in `seq 0 3`; do
+  for i in `seq 0 1`; do
     echo "${track}|mlrtdp|hor=${horizon}"
       ../testsolver.out --track=../data/tracks/$track.track \
       --algorithm=mlrtdp --horizon=$horizon --v=0 --n=$nsims \
@@ -37,7 +37,7 @@ for track in ${tracks[@]}; do
   --heuristic=hmin --hmin-solve-all 
       
   j=1
-  for i in `seq 0 3`; do
+  for i in `seq 0 1`; do
     echo "${track}|hdp(0,j)|j=${j}"
       ../testsolver.out --track=../data/tracks/$track.track \
       --algorithm=hdp --i=0 --j=$j --v=0 --n=$nsims \
