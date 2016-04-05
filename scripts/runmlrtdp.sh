77e848e04bd8fb5246bddd68d1ca01bd293dd7ff
Luis Pineda <luisen.p@gmail.com>
2016-04-05 12:54:03 -0400

    added a few more racetracks

--- scripts/runmlrtdp.sh
+++ scripts/runmlrtdp.sh
@@ -12,13 +12,13 @@ for track in ${tracks[@]}; do
   
   echo "${track}|mlrtdp"
   ../testsolver.out --track=../data/tracks/$track.track \
-  --algorithm=mlrtdp --v=0 --n=1 --optimal --no-initial-plan \
+  --algorithm=mlrtdp --v=0 --n=1 --optimal  \
   --heuristic=hmin --hmin-solve-all 
   
   # Approximate MLRDP 
   echo "${track}|mlrtdp|hor=0"
   ../testsolver.out --track=../data/tracks/$track.track \
-  --algorithm=mlrtdp --horizon=0 --v=0 --n=$nsims --no-initial-plan \
+  --algorithm=mlrtdp --horizon=0 --v=0 --n=$nsims  \
   --heuristic=hmin --hmin-solve-all 
       
   horizon=1
@@ -26,22 +26,22 @@ for track in ${tracks[@]}; do
     echo "${track}|mlrtdp|hor=${horizon}"
       ../testsolver.out --track=../data/tracks/$track.track \
       --algorithm=mlrtdp --horizon=$horizon --v=0 --n=$nsims \
-      --no-initial-plan --heuristic=hmin --hmin-solve-all 
+       --heuristic=hmin --hmin-solve-all 
     let "horizon *= 2"
   done
-  
-  # Approximate HDP(0,j) 
+    
+  # Approximate HDP(0,j)
   echo "${track}|hdp(0,j)|j=0"
   ../testsolver.out --track=../data/tracks/$track.track \
-  --algorithm=mlrtdp --horizon=0 --v=0 --n=$nsims --no-initial-plan \
+  --algorithm=hdp --i=0 --j=0 --v=0 --n=$nsims  \
   --heuristic=hmin --hmin-solve-all 
-      
+   
   j=1
   for i in `seq 0 1`; do
     echo "${track}|hdp(0,j)|j=${j}"
       ../testsolver.out --track=../data/tracks/$track.track \
       --algorithm=hdp --i=0 --j=$j --v=0 --n=$nsims \
-      --no-initial-plan --heuristic=hmin --hmin-solve-all 
+       --heuristic=hmin --hmin-solve-all 
     let "j *= 2"
   done
   
