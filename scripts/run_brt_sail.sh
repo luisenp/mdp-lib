16dde18ae3dbd8422db662475c02d3c271796270
Luis Pineda <luisen.p@gmail.com>
2016-04-11 12:04:01 -0400

    added a script for sailing domain experiments

--- scripts/run_brt_sail.sh
+++ scripts/run_brt_sail.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-sizes=(10 20)
+sizes=(20 40)
 nsims=100
 
 for size in ${sizes[@]}; do
+  let "goal = size - 1"
   # LRTDP
-  echo "${size}|lrtdp"
-  ../testsolver.out --sailing-size=20 --sailing-goal=10 --algorithm=lrtdp \
+  echo "${size}|$goal|lrtdp"
+  ../testsolver.out --sailing-size=$size --sailing-goal=$goal --algorithm=lrtdp \
   --horizon=2 --i=0 --j=1 --n=1 --v=100 --heuristic=hmin --hmin-solve-all
   
   # Approximate MLRDP
   for horizon in `seq 0 2`; do
-    echo "${size}|mlrtdp|hor=${horizon}"
-    ../testsolver.out --sailing-size=20 --sailing-goal=10 --algorithm=mlrtdp \
+    echo "${size}|$goal||mlrtdp|hor=${horizon}"
+    ../testsolver.out --sailing-size=$size --sailing-goal=$goal --algorithm=mlrtdp \
     --horizon=$horizon --n=$nsims --v=0 --heuristic=hmin --hmin-solve-all
   done
     
   # Approximate HDP(0,j)
   for i in `seq 0 1`; do
     for j in `seq 0 1`; do
-      echo "${size}|hdp|hor=${horizon}"
-      ../testsolver.out --sailing-size=20 --sailing-goal=10 --algorithm=hdp \
+      echo "${size}|$goal||hdp|i=${i},j={$j}"
+      ../testsolver.out --sailing-size=$size --sailing-goal=$goal --algorithm=hdp \
         --horizon=$horizon --n=$nsims --v=0 -i=$i j=$j \
         --heuristic=hmin --hmin-solve-all
     done
@@ -29,8 +30,8 @@ for size in ${sizes[@]}; do
   # Approximate SSiPP
   horizon=1
   for i in `seq 0 3`; do
-    echo "${size}|ssipp|hor=${horizon}"
-    ../testsolver.out --sailing-size=20 --sailing-goal=10 --algorithm=hdp \
+    echo "${size}|$goal||ssipp|hor=${horizon}"
+    ../testsolver.out --sailing-size=$size --sailing-goal=$goal --algorithm=hdp \
       --horizon=$horizon --n=$nsims --v=0 -i=$i j=$j \
       --heuristic=hmin --hmin-solve-all      
       let "horizon *= 2"
