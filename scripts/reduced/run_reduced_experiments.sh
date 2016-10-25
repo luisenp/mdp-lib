6b9649e67e136f8920d7f169d61e41bce3eacbdc
Luis Pineda <luisen.p@gmail.com>
2016-10-25 15:52:12 -0400

    moved the triangle-tireworld problems to a new folder

--- scripts/reduced/run_reduced_experiments.sh
+++ scripts/reduced/run_reduced_experiments.sh
@@ -10,16 +10,16 @@
 # the problem name in the file must match the PPDDL filename.
 #
 # pddl_folder=/home/lpineda/Desktop/skeren
-# pddl_folder=../../data/ppddl/ippc2008
-pddl_folder=../../data/ppddl/ippc2006
+pddl_folder=../../data/ppddl/ippc2008
+# pddl_folder=../../data/ppddl/ippc2006
 
 # The domain name.
-# domain=triangle-tireworld
+domain=triangle-tireworld
 # domain=zenotravel
 # domain=boxworld
 # domain=blocksworld
 # domain=ex-blocksworld
-domain=elevators
+# domain=elevators
 
 # The name of the problem used to learn the best determinization. 
 problem=p01
@@ -28,7 +28,7 @@ problem=p01
 k=0
 
 # The name of the domain file to determinize
-domain_file_name=domain
+domain_file_name=domain-design-all
 
 # Creating all possible determinizations
 ./create_all_determinizations.py -d $pddl_folder/$domain/$domain_file_name.pddl \
