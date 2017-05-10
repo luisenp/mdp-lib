e1c1654fee34597b308a8caa59ec93f2362a745a
Luis Pineda <luisen.p@gmail.com>
2017-05-10 11:33:04 -0400

    fixed an error in the racetrack domain (relating to the MDS). Added time count to mklreplan

--- scripts/runjair17_1.sh
+++ scripts/runjair17_1.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-verbosity=1000
-nsims=1000
+verbosity=0
+nsims=100
+mdsval=2
+
+echo "**** map3.track ****"
+echo "++ vi"
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map3.track --v=$verbosity --n=$nsims --debug --mds=$mdsval --use-full --use-vi
+echo "++ lao"
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map3.track --v=$verbosity --n=$nsims --debug --mds=$mdsval --use-full
+echo "++ det"
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map3.track --k=0 --v=$verbosity --n=$nsims --best-det-racetrack-greedy --debug --mds=$mdsval
+echo "++ m02"
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map3.track --k=0 --v=$verbosity --n=$nsims --best-m02-racetrack-greedy --debug --mds=$mdsval
+echo "++ m11"
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map3.track --k=1 --v=$verbosity --n=$nsims --best-det-racetrack-greedy --debug --mds=$mdsval
+echo "++ m12"
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map3.track --k=1 --v=$verbosity --n=$nsims --best-m02-racetrack-greedy --debug --mds=$mdsval
 
 echo "**** map4.track ****"
 echo "++ vi"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --v=$verbosity --n=$nsims --debug --use-full --use-vi
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --v=$verbosity --n=$nsims --debug --mds=$mdsval --use-full --use-vi
 echo "++ lao"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --v=$verbosity --n=$nsims --debug --use-full
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --v=$verbosity --n=$nsims --debug --mds=$mdsval --use-full
 echo "++ det"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=0 --v=$verbosity --n=$nsims --best-det --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=0 --v=$verbosity --n=$nsims --best-det-racetrack-greedy --debug --mds=$mdsval
 echo "++ m02"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=0 --v=$verbosity --n=$nsims --best-m02 --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=0 --v=$verbosity --n=$nsims --best-m02-racetrack-greedy --debug --mds=$mdsval
 echo "++ m11"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=1 --v=$verbosity --n=$nsims --best-det --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=1 --v=$verbosity --n=$nsims --best-det-racetrack-greedy --debug --mds=$mdsval
 echo "++ m12"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=1 --v=$verbosity --n=$nsims --best-m02 --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map4.track --k=1 --v=$verbosity --n=$nsims --best-m02-racetrack-greedy --debug --mds=$mdsval
 
 echo "**** map5.track ****"
 echo "++ vi"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --v=$verbosity --n=$nsims --debug --use-full --use-vi
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --v=$verbosity --n=$nsims --debug --mds=$mdsval --use-full --use-vi
 echo "++ lao"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --v=$verbosity --n=$nsims --debug --use-full
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --v=$verbosity --n=$nsims --debug --mds=$mdsval --use-full
 echo "++ det"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=0 --v=$verbosity --n=$nsims --best-det --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=0 --v=$verbosity --n=$nsims --best-det-racetrack-greedy --debug --mds=$mdsval
 echo "++ m02"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=0 --v=$verbosity --n=$nsims --best-m02 --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=0 --v=$verbosity --n=$nsims --best-m02-racetrack-greedy --debug --mds=$mdsval
 echo "++ m11"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=1 --v=$verbosity --n=$nsims --best-det --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=1 --v=$verbosity --n=$nsims --best-det-racetrack-greedy --debug --mds=$mdsval
 echo "++ m12"
-../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=1 --v=$verbosity --n=$nsims --best-m02 --debug
+../testreduced.out --domain=racetrack --problem=../data/tracks/roads-huge/map5.track --k=1 --v=$verbosity --n=$nsims --best-m02-racetrack-greedy --debug --mds=$mdsval
