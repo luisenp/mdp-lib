69919a942a2efb2d6f4435d40fa2c78321978f33
Luis Pineda <luisen.p@gmail.com>
2017-09-05 13:53:49 -0400

    cleaned up the UCT code

--- src/solvers/UCTSolver.cpp
+++ src/solvers/UCTSolver.cpp
@@ -12,10 +12,8 @@ mlcore::Action* UCTSolver::pickAction(UCTNode* node, double C)
     double best = mdplib::dead_end_cost + 1;
     mlcore::Action* bestAction = nullptr;
     std::vector<mlcore::Action*> unexplored_actions;
-                                                                                dprint2("  ==pick action", node);
     auto actions_qvalue_map = action_qvalues_[node];
     int num_actions = actions_qvalue_map.size();
-                                                                                dprint2("  num actions", num_actions);
     for (auto action_and_qvalue : actions_qvalue_map) {
         mlcore::Action* a = action_and_qvalue.first;
         if (!problem_->applicable(node->state_, a))
@@ -24,13 +22,11 @@ mlcore::Action* UCTSolver::pickAction(UCTNode* node, double C)
             unexplored_actions.push_back(a);
             continue;
         }
-                                                                                dprint2("    cost for", a);
         if (use_qvalues_for_c_) {
             C = action_and_qvalue.second;
             C /= sqrt(log(num_actions) / (delta_ + 1) + 1);
         }
         double ucb1 = ucb1Cost(node, a, C);
-                                                                                dprint2("    ucb1cost =", ucb1);
         if (ucb1 < best) {
             bestAction = a;
             best = ucb1;
@@ -38,19 +34,13 @@ mlcore::Action* UCTSolver::pickAction(UCTNode* node, double C)
     }
     if (unexplored_actions.size() > 0) {
         int idx = rand() % unexplored_actions.size();
-                                                                                dprint2("    unexplored", unexplored_actions[idx]);
         return unexplored_actions[idx];
     }
-                                                                                dprint2("    best", bestAction);
     return bestAction;
 }
 
 double UCTSolver::ucb1Cost(UCTNode* node, mlcore::Action* a, double C)
 {
-                                                                                dprint4("      counters (node, action) / qvalue =",
-                                                                                        counters_node_[node],
-                                                                                        counters_node_action_[node][a],
-                                                                                        action_qvalues_[node][a]);
     double cost = action_qvalues_[node][a] - C
         * std::sqrt(std::log(counters_node_[node])
                     / counters_node_action_[node][a]);
@@ -70,7 +60,6 @@ mlcore::Action* UCTSolver::solve(mlcore::State* s0)
             bool first_time_seen = visited_.insert(tmp_node).second;
             if (first_time_seen) {
                 counters_node_[tmp_node] = 0;
-                                                                                dprint2("count", counters_node_.size());
                 for (mlcore::Action* a : problem_->actions()) {
                     if (!problem_->applicable(tmp_node->state_, a))
                         continue;
@@ -90,7 +79,6 @@ mlcore::Action* UCTSolver::solve(mlcore::State* s0)
             nodes_in_rollout[i] = tmp_node;
             actions_in_rollout[i] = a;
             tmp_node = new UCTNode(next, tmp_node->depth_ + 1);
-                                                                                dprint2("  ==next", tmp_node);
         }
 
         for (int i = 1; i <= maxSteps; i++) {
@@ -100,15 +88,10 @@ mlcore::Action* UCTSolver::solve(mlcore::State* s0)
                 * action_qvalues_[node][a]
                 + cumCost[maxSteps] - cumCost[i - 1];
             newq /= (counters_node_action_[node][a] + 1);
-                                                                                dprint3("update-qvalue", node, a);
-                                                                                dprint3(counters_node_action_[node][a],
-                                                                                        newq,
-                                                                                        cumCost[maxSteps] - cumCost[i - 1]);
             action_qvalues_[node][a] = newq;
             counters_node_[node]++;
             counters_node_action_[node][a]++;
         }
-                                                                                dprint1("**************************************");
     }
 
     if (auto_adjust_depth_) {
