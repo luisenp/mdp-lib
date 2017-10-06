638f3cdac94aab3c628b88047cfb76ce3593742d
Luis Pineda <luisen.p@gmail.com>
2017-10-06 05:49:41 -0400

    minor changes

--- scripts/test_outsel.sh
+++ scripts/test_outsel.sh
@@ -1,5 +1,5 @@
 n=10000
-sz=8
+sz=10
 for ntr in `seq 10 10 100`; do
   echo $ntr
   ../testthts.out --randtree=$sz --backup=pb --action-sel=ucb1 --v=0 --n=$n --horizon=100 --out-sel=tran --trials=$ntr
