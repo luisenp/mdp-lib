db0e6e3090f170906f92abe819c31aa9d910031b
Luis Pineda <luisen.p@gmail.com>
2017-10-06 05:54:56 -0400

    added some racetrack experiments to the test_outsel script

--- scripts/test_outsel.sh
+++ scripts/test_outsel.sh
@@ -1,7 +1,18 @@
-n=10000
-sz=8
-for ntr in `seq 10 10 100`; do
-  echo $ntr
-  ../testthts.out --randtree=$sz --backup=pb --action-sel=ucb1 --v=0 --n=$n --horizon=100 --out-sel=tran --trials=$ntr
-  ../testthts.out --randtree=$sz --backup=pb --action-sel=ucb1 --v=0 --n=$n --horizon=100 --out-sel=min-var --trials=$ntr
+# n=10000
+# sz=8
+# for ntr in `seq 10 10 100`; do
+#   echo $ntr
+#   ../testthts.out --randtree=$sz --backup=pb --action-sel=ucb1 --v=0 --n=$n --horizon=100 --out-sel=tran --trials=$ntr
+#   ../testthts.out --randtree=$sz --backup=pb --action-sel=ucb1 --v=0 --n=$n --horizon=100 --out-sel=min-var --trials=$ntr
+# done
+
+tracks=(known/square-2-error known/ring-3-error)
+nsims=1000
+for track in ${tracks[@]}; do
+  echo $track
+  for ntrials in `seq 10 10 200`; do
+    echo $ntrials
+    ../testthts.out --track=../data/tracks/$track.track --backup=pb --action-sel=ucb1 --v=0 --n=$nsims --horizon=50 --out-sel=tran --trials=$ntrials
+    ../testthts.out --track=../data/tracks/$track.track --backup=pb --action-sel=ucb1 --v=0 --n=$nsimsn --horizon=50 --out-sel=min-var --trials=$ntrials
+  done
 done
