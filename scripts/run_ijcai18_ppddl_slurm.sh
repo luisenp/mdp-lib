500dd036efd69fdad69c04b41d8065c146909d58
Luis Pineda <luisen.p@gmail.com>
2018-03-21 21:55:44 -0400

    fixed some errors in the slurm script

--- scripts/run_ijcai18_ppddl_slurm.sh
+++ scripts/run_ijcai18_ppddl_slurm.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
+# Usage ./run_ijcai18_ppddl_slurm.sh
 
-# The folder where the PPDDL files are stored. The contents should be 
-# structured as follows:
-#
-# pddl_folder/domain-name-1/*.pddl
-# pddl_folder/domain-name-2/*.pddl
-#
-# The PPDDL files must contain both domain and problem definitions and
-# the problem name in the file must match the PPDDL filename.
-#
-pddl_folder=$1
+ppddl_folder=/home/lpineda/mdp-lib/data/ppddl/ippc2008
+domain=ex-blocksworld-fixed
 
-# The domain name.
-domain=$2
-
-# Parameters
-ntrials=1000
-nsims=50
-verbosity=0
-depth=10
-alpha=0.3
-dist=traj
-labelf=linear
-
-# Starts the planning server to connect to mdpsim
 for i in {01..10}; do
   problem=p$i
   sbatch --output=/home/lpineda/results_ijcai18/ppddl/${domain}_${problem}.txt \
-    time ../testppddl.out $pddl_folder/$domain/$problem.pddl $problem \
-    $ntrials $nsims $verbosity --algorithm=soft-flares \
-    --depth=$depth --alpha=$alpha --dist=$dist --labelf=$labelf &
+    run_testppddl_softflares.sh ${ppddl_folder} ${domain} ${problem}
 done
 
---
+++ scripts/run_testppddl_softflares.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Usage [pddl_folder] [domain] [problem]
+
+# The folder where the PPDDL files are stored. The contents should be 
+# structured as follows:
+#
+# pddl_folder/domain-name-1/*.pddl
+# pddl_folder/domain-name-2/*.pddl
+#
+# The PPDDL files must contain both domain and problem definitions and
+# the problem name in the file must match the PPDDL filename.
+#
+pddl_folder=$1
+
+# The domain name.
+domain=$2
+
+# The problem number
+problem=$3
+
+# Parameters
+ntrials=1000
+nsims=50
+verbosity=10
+depth=10
+alpha=0.1
+dist=traj
+labelf=logistic
+
+timeout 20m time ../testppddl.out $pddl_folder/$domain/$problem.pddl $problem \
+  $ntrials $nsims $verbosity --algorithm=soft-flares \
+  --depth=$depth --alpha=$alpha --dist=$dist --labelf=$labelf
+
