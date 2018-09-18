610fe105ef344e94f043844b07b5a1fb23099aeb
Luis Pineda <luisen.p@gmail.com>
2018-09-18 14:31:16 -0400

    fixed errors in script for JAIR

---
+++ scripts/run_testreduced_racetrack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Usage ./run_testsolver.sh [track] [k_reduced] [nsims] [model] [maxt] [extraflags]
+
+track=$1
+k_reduced=$2
+nsims=$3
+model=$4
+maxt=$5
+extraflags=$6
+
+echo "--domain=racetrack --problem=../data/tracks/${track}.track --perror=.20 --pslip=0.40 --k=${k_reduced} --n=${nsims} --${model} --maxt=${maxt} --v=0 --mds=-1 --use-lrtdp ${extraflags}"
+
+../testreduced.out --domain=racetrack \
+  --problem=../data/tracks/${track}.track --perror=.20 --pslip=0.40 \
+  --k=${k_reduced} --n=${nsims} --${model} --maxt=${maxt} \
+  --v=0 --mds=-1 ${extraflags}
+  
--- scripts/runjair19.sh
+++ scripts/runjair19.sh
@@ -11,18 +11,20 @@ times=(50 100 200 400 800 1600 3200)
 save_dir="/home/lpineda/results_jair/anytime_racetrack"        
 
 for ((id_track = 0; id_track < ${#tracks[@]}; id_track++)); do
-  track=${tracks[$id_track]}  
-  for ((id_model = 0; id_model < ${#models[@]}; id_model++)); do
-    model=${models[$id_model]}    
-    for k_reduced in `seq 0 2`; do
-      for ((id_time = 0; id_time < ${#times[@]}; id_time++)); do
+  track=${tracks[$id_track]}
+  for ((id_time = 0; id_time < ${#times[@]}; id_time++)); do    
+    maxt=${times[$id_time]}
+    output_file=${save_dir}/${track}.fullmodel.t${maxt}
+    sbatch --output=${output_file} \
+      ./run_testreduced_racetrack.sh \
+        ${track} "0" ${nsims} "no-mkl" ${maxt} "--use-full"
+    for ((id_model = 0; id_model < ${#models[@]}; id_model++)); do
+      model=${models[$id_model]}
+      for k_reduced in `seq 0 2`; do
         maxt=${times[$id_time]}
         output_file=${save_dir}/${track}.${model:5:3}.k${k_reduced}.t${maxt}
-        echo ${output_file}
-        sbatch --output=${output_file} ../testreduced.out --domain=racetrack \
-          --problem=../data/tracks/${track}.track --perror=.20 --pslip=0.40 \
-          --k=${k_reduced} --n=${nsims} --${model} --maxt=${maxt} \
-          --v=0 --mds=-1 --use-lrtdp --use-full
+        sbatch --output=${output_file} \
+          ./run_testreduced_racetrack.sh ${track} ${k_reduced} ${nsims} ${model} ${maxt}
       done
     done  
   done
