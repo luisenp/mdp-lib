7089f81cb9d7f58de58f500c0bfce053781e4171
Luis Pineda <luisen.p@gmail.com>
2018-09-19 11:10:12 -0400

    minor changes to slurm script

--- scripts/runjair19.sh
+++ scripts/runjair19.sh
@@ -6,7 +6,7 @@ tracks=("roads-huge/map3" \
 
 models=("best-det-racetrack-greedy" "best-m02-racetrack-greedy")
 
-times=(50 100 200 400 800 1600 3200)
+times=(50 100 200 400 800 1600 3200 6400)
 
 save_dir="/home/lpineda/results_jair/anytime_racetrack"        
 
@@ -15,17 +15,27 @@ for ((id_track = 0; id_track < ${#tracks[@]}; id_track++)); do
   for ((id_time = 0; id_time < ${#times[@]}; id_time++)); do    
     maxt=${times[$id_time]}
     output_file=${save_dir}/${track}.fullmodel.t${maxt}
+    # This runs full-model planning
     sbatch --output=${output_file} \
       ./run_testreduced_racetrack.sh \
         ${track} "0" ${nsims} "no-mkl" ${maxt} "--use-full"
+    # This loops different reduced models and values of k
     for ((id_model = 0; id_model < ${#models[@]}; id_model++)); do
       model=${models[$id_model]}
-      for k_reduced in `seq 0 2`; do
+      for k_reduced in `seq 0 3`; do
         maxt=${times[$id_time]}
         output_file=${save_dir}/${track}.${model:5:3}.k${k_reduced}.t${maxt}
         sbatch --output=${output_file} \
           ./run_testreduced_racetrack.sh ${track} ${k_reduced} ${nsims} ${model} ${maxt}
       done
+      # Run a planner that increases k when current state is already solved
+      # (Only starting with k=1)
+      maxt=${times[$id_time]}
+      output_file=${save_dir}/${track}.${model:5:3}.kincrease.t${maxt}
+      sbatch --output=${output_file} \
+        ./run_testreduced_racetrack.sh \
+          ${track} "1" ${nsims} ${model} ${maxt} "--increase_k"
+          
     done  
   done
 done
