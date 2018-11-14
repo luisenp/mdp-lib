8d24aae20d456e165f6268abdaf8a9e0803620f3
Luis Pineda <luisen.p@gmail.com>
2018-11-14 15:27:57 -0500

    fixed minor bug in slurm script

--- scripts/run_aaai19_slurm.sh
+++ scripts/run_aaai19_slurm.sh
@@ -72,7 +72,7 @@ for ((ip = 0; ip < ${#problems[@]}; ip++)); do
   
   # ---- SSiPP and FLARES
   ssipp_hor=1
-  for $horizon in `seq 1 4`; do
+  for horizon in `seq 1 4`; do
     # Depth-based SSiPP(1-4)
     sbatch ${swarm_flags} --output=${output_dir}/${problem_str}_"ssipp_$ssipp_hor".txt \
       run_testsolver.sh "$problem" $nsims $reps $verbosity $min_time $max_time "$other_flags" \
