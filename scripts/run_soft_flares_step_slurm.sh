f34f425c68b0d3697abf183902daaa4f8698bd96
Luis Pineda <luisen.p@gmail.com>
2018-11-14 23:00:17 -0500

    added a script to run multiple multiple alphas with soft flares and the step function

---
+++ scripts/run_soft_flares_step_slurm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Usage ./run_soft_flares_step_slurm.sh 
+
+nsims=3
+reps=1
+verbosity=-1
+min_time=-1
+max_time=-1
+other_flags="--reset-every-trial"
+
+problems=( "--track=../data/tracks/known/square-4-error.track --perror=0.10 --pslip=0.20" \
+           "--track=../data/tracks/known/ring-5-error.track --perror=0.10 --pslip=0.20" \
+           "--sailing-size=40 --sailing-goal=39" \
+           "--sailing-size=40 --sailing-goal=20")
+
+problems_str=(square4 ring5 sailing-corner sailing-middle)
+alphas=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
+
+output_dir="/home/lpineda/results_aamas19/until_stops_${nsims}_sims/flares_soft"
+swarm_flags="--partition=longq --time=10-01:00:00"
+for ((ip = 0; ip < ${#problems[@]}; ip++)); do
+  problem=${problems[$ip]}
+  problem_str=${problems_str[$ip]}  
+  for alpha in ${alphas[@]}; do
+    for horizon in `seq 1 4`; do
+      sbatch ${swarm_flags} --output=${output_dir}/${problem_str}_"$alpha_$horizon".txt \
+        run_testsolver.sh "$problem" $nsims $reps $verbosity $min_time $max_time "$other_flags" \
+        "flares_soft --labelf=step --dist=depth --horizon=$horizon --alpha=0"
+    done
+  done    
+done
